66f4a20e7d7db9d2b832c73e93341bed8230979d
MintKK <30825268+MintKK@users.noreply.github.com>
2017-12-12 12:09:32 +0800

    Check MKK Changelog for list of prominent changes, changed URL routing method and updated navbar routes + minor error prevention. (Re-commit due to conflict error)

--- CommuHub/Time_Functions.py
+++ CommuHub/Time_Functions.py
@@ -2,4 +2,4 @@ import datetime
 
 def returnCurrentDate():
     currentdatetime = datetime.datetime.now()
-    return (str(currentdatetime.day) + "-" + str(currentdatetime.month) + "-" + str(currentdatetime.year))
+    return (str(currentdatetime.day) + "/" + str(currentdatetime.month) + "/" + str(currentdatetime.year))
